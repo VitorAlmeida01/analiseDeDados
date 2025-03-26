ffbe9902119644aa50afda4f7faa70b119a40cd1
LucasDev-end <lucas.marcolino@sptech.school>
2025-03-25 21:42:11 -0300

    Criando novo arquivo .R

---
+++ AtividadeConvenio2.0.R
@@ -0,0 +1,29 @@
+# Convênio de Saúde “Fique em Paz”
+# cerca de 32% atendimento na rede de pronto socorro por problemas respiratórios.
+# previsão de cerca de 4 semanas para uma onda de calor na cidade de são paulo.
+# faixa etária desses pacientes estão entre 7 a 17 e 55 a 82 anos.
+# elabore a simulação, criando um modelo de distribuição normal para este cenário,
+# com o objetivo de responder as perguntas dos executivos que administram o hospital.
+
+# 1- Qual a média de idade com maior número de atendimentos médicos?
+
+# 2- Quais faixas etárias necessitam de maior número de atendimentos médicos?
+
+# 3- Qual ala, os hospital deve priorizar o atendimento, jovens ou idosos?
+
+# 4- Qual o percentual da ala que precisa ser priorizada?
+
+# 5– Todas as perguntas acima devem ser justificadas por meio de gráficos.
+
+# 6– Elabore uma apresentação para que os executivos do Convênio, possam compreender os insights elaborados
+
+#A Simulação deve ser feita com a linguagem R. 
+
+#Para realizar esta atividade a dupla deve entrar em acordo e defender sua simulação.
+
+#Troque sua apresentação com a de outro grupo depois que estiver finalizada. 
+# O grupo que defendeu o projeto deverá contra argumentar o projeto do outro grupo,
+# e o grupo que recebeu o contra argumento, além de defender o seu projeto, irá contra argumentar o grupo.
+
+#Boa sorte!
+
