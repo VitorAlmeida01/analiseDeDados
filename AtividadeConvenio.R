4488a09a909dcb5302a11273f5afd5e6338cbf6d
VitorAlmeida01 <vitor.almeida@sptech.school>
2025-03-25 22:38:23 -0300

    versao final

--- AtividadeConvenio.R
+++ AtividadeConvenio.R
@@ -4,6 +4,8 @@ n <- 60480
 n17 <- n / 2
 n55 <- n / 2
 
+set.seed(1245)
+
 # idadepop <- abs(round(rnorm(n, 37, 7), 0))
 # idadepop
 
@@ -14,8 +16,17 @@ idade55a82 <- sample(55:82, n55, replace = TRUE)
 idade55a82
 
 idades <- c(idade7a17, idade55a82)
-
 idades
+
+ala_temp <- rbinom(n, 1, 0.30)
+ala_temp
+
+ala <- factor(ala_temp,
+              levels = c(0,1),
+              labels = c("Geral", "Socorro"),
+              ordered = TRUE
+)
+ala
 
 atendimento_temp <- rbinom(n, 1, 0.30)
 atendimento_temp
@@ -26,28 +37,45 @@ atendimento <- factor(atendimento_temp,
                   ordered = TRUE
 )
 atendimento
-
-df = data.frame(idades,atendimento, atendimento_temp)
+# Data frame
+df = data.frame(idades,atendimento, atendimento_temp, ala)
 
 View(df)
 
 mediaAtendida <- round(mean(df$atendimento_temp), 2)
+# Qual a média de idade com maior número de atendimentos médicos?
 mediaAtendida
-barplot(table(df$atendimento), 
-        col = c("red", "blue"), 
-        main = "Distribuição de Atendimentos", 
-        xlab = "Atendimento", 
-        ylab = "Quantidade de Pessoas")
-
 
 df_atendidos <- df[df$atendimento_temp == 1, ]
 media_idade_a <- round(mean(df_atendidos$idades), 0)
+# Quais faixas etárias necessitam de maior número de atendimentos médicos?
 media_idade_a
 
-hist(df_atendidos$idades, 
-     breaks = 15, 
-     col = "lightblue", 
-     main = "Distribuição de Idades dos Atendidos",
-     xlab = "Idade",
-     ylab = "Frequência")
+ala_counts <- table(df_atendidos$ala)
+ala_counts
+
+
+# Qual ala, os hospital deve priorizar o atendimento, jovens ou idosos?
+df$idades <- as.numeric(df$idades)
+tabela_faixa <- table(cut(df$idades, breaks = c(6, 17, 54, 82),
+                          labels = c("Jovens (7-17)", 
+                                     "Adultos (18-54)", 
+                                     "Idosos (55-82)")))
+print(tabela_faixa)
+
+
+# Qual o percentual da ala que precisa ser priorizada?
+barplot(as.numeric(ala_counts),
+        col = c("blue", "red"),
+        main = "Atendimentos por Ala",
+        xlab = "Ala",
+        ylab = "Quantidade de Atendimentos",
+        names.arg = names(ala_counts))
+
+
+
+
+
+
+
 
